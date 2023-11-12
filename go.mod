1de618b85fc7904d1db32292711f4ab9fd3d2989
Febriansyah <febrian@efner.id>
2023-11-12 12:57:53 +0700

    first init
    setting all posible domains

---
+++ go.mod
@@ -0,0 +1,8 @@
+module coffeeco
+
+go 1.21.4
+
+require (
+	github.com/Rhymond/go-money v1.0.10
+	github.com/google/uuid v1.4.0
+)
---
+++ internal/purchase/purchase.go
@@ -0,0 +1,43 @@
+package purchase
+
+import (
+	coffeeco "coffeeco/internal"
+	"coffeeco/internal/payment"
+	"coffeeco/internal/store"
+	"errors"
+	"time"
+
+	"github.com/Rhymond/go-money"
+	"github.com/google/uuid"
+)
+
+type Purchase struct {
+	id                 uuid.UUID
+	Store              store.Store
+	ProductsToPurchase []coffeeco.Product
+	total              money.Money
+	PaymentMeans       payment.Means
+	timeOfPurchase     time.Time
+	CardToken          *string
+}
+
+func (p *Purchase) validateAndEnrich() error {
+	if len(p.ProductsToPurchase) == 0 {
+		return errors.New("purchase must consist of at least one product")
+	}
+
+	p.total = *money.New(0, "USD")
+	for _, v := range p.ProductsToPurchase {
+		newTotal, _ := p.total.Add(&v.BasePrice)
+		p.total = *newTotal
+	}
+
+	if p.total.IsZero() {
+		return errors.New("likely mistake;purchase should never be 0, please validate")
+	}
+
+	p.id = uuid.New()
+	p.timeOfPurchase = time.Now()
+
+	return nil
+}
